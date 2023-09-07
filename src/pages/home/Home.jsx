9e2e75541fb447bdb22c14ccf4a2948c49ea18fa
Pramuditha Jayasinghe <pramuditha.niroshan@circles.asia>
2023-09-07 18:22:00 +0530

    feat: Technologies component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import PortfolioCard from "../../components/portfolioCard/PortfolioCard";
+import Technologies from "../technologies/Technologies";
 
 const name = "John Doe";
 const aboutme = "Experienced web developer creating engaging websites. Let's bring your digital vision to life!"
 
 const Home = () => {
+  const technologies = ["NextJS", "React", "Tailwind CSS", "Figma", "Express"];
   return <div>
+      <Technologies title="Technologies" technologies={technologies}/>
     <PortfolioCard name={name} aboutme={aboutme} />
   </div>
 };
--- src/pages/technologies/Technologies.jsx
+++ src/pages/technologies/Technologies.jsx
@@ -1,5 +1,16 @@
-const Technologies = () => {
-  return <div>Technologies</div>;
+import "./Technologies.css";
+
+const Technologies = ({ title, technologies }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <ul>
+        {technologies.map((item) => (
+          <li className="tech-item" key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Technologies;
