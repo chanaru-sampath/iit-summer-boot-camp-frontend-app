dd9134c9182e0f000b92a06c9dc78e02165059d5
Gayan Kumarage <gayan.kumarage@circles.asia>
2023-09-07 18:17:15 +0530

    feat: portfolio section initial draft

--- src/components/portfolioCard/PortfolioCard.jsx
+++ src/components/portfolioCard/PortfolioCard.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import profile from "../../assets/profile.png"
+import github from "../../assets/github.png"
+import instagram from "../../assets/instagram.png"
+import linkedin from "../../assets/linkedin.png"
+import twitter from "../../assets/twitter.png"
+import "./PortfolioCard.css";
 
-function PortfolioCard() {
+function PortfolioCard(props) {
+  const {
+    name,
+    aboutme
+  } = props;
+
   return (
-    <div>PortfolioCard</div>
+    <div>
+      <section className="aboutme">
+        <div className="title">
+          <h1>{name}</h1>
+          <p>{aboutme}</p>
+        </div>
+      <div className="prof-image">
+        <img src={profile} style={{height: "257px", padding: "8px", alt: "profile image"}} />
+      </div>
+      </section>
+      <section>
+       <img src={github} style={{height: "24px", padding: "6px", alt: "github logo"}} />
+       <img src={instagram} style={{height: "24px", padding: "6px", alt: "instagram logo"}} />
+       <img src={linkedin} style={{height: "24px", padding: "6px", alt: "linkedin logo"}} />
+       <img src={twitter} style={{height: "24px", padding: "6px", alt: "twitter logo"}} />
+      </section>
+      <button>More About Me</button>
+    </div>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
 import PortfolioCard from "../../components/portfolioCard/PortfolioCard";
 
+const name = "John Doe";
+const aboutme = "Experienced web developer creating engaging websites. Let's bring your digital vision to life!"
+
 const Home = () => {
   return <div>
-    <PortfolioCard />
+    <PortfolioCard name={name} aboutme={aboutme} />
   </div>;
 };
 
