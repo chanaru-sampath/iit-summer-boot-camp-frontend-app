a095f79444ad34e8ae41207472bed1aae725a8a8
Gayan Kumarage <gayan.kumarage@circles.asia>
2023-09-07 19:13:25 +0530

    styles: updated list item title

--- src/components/listItems/ListItems.jsx
+++ src/components/listItems/ListItems.jsx
@@ -3,7 +3,7 @@ import "./ListItems.css";
 const ListItems = ({ title, items }) => {
   return (
     <div>
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       <ul>
         {items.map((item) => (
           <li className="list-item" key={item}>{item}</li>
