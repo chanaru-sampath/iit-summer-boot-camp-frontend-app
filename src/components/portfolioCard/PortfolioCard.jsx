dc2ea49ab43dda8286171551991644a1df01bf88
Pramuditha Jayasinghe <pramuditha.niroshan@circles.asia>
2023-09-07 19:38:29 +0530

    feat: Portfolio Card component

--- src/components/portfolioCard/PortfolioCard.jsx
+++ src/components/portfolioCard/PortfolioCard.jsx
@@ -1,7 +1,23 @@
 import "./PortfolioCard.css";
 
-function PortfolioCard() {
-  return <div className="portfolio-container">Portforlio Card</div>;
+function PortfolioCard(props) {
+  const { cardImage, title, description, likes } = props;
+
+  return (
+    <div className="portfolio-card">
+      <div className="card-image">
+        <img src={cardImage} alt={`image-${title}`} />
+      </div>
+      <div className="card-details">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+      <div className="card-actions">
+        <button>Like</button>
+        <h6>{likes}</h6>
+      </div>
+    </div>
+  );
 }
 
 export default PortfolioCard;
