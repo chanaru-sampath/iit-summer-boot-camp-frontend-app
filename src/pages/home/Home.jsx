9de25d7c1d33586735c5246864264727aac3c54c
Gayan Kumarage <gayan.kumarage@circles.asia>
2023-09-07 19:07:47 +0530

    styles: updated list item styles

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,11 +5,13 @@ import PortfolioCard from "../../components/portfolioCard/PortfolioCard";
 const name = "John Doe";
 const aboutme = "Experienced web developer creating engaging websites. Let's bring your digital vision to life!"
 const technologies = ["NextJS", "React", "Tailwind CSS", "Figma", "Express"];
+const projects = ["NextJS", "React", "Express"];
 
 const Home = () => {
   return <div>
     <HeroSection name={name} aboutme={aboutme} />
     <ListItems title="Technologies" items={technologies}/>
+    <ListItems title="Portfolio" items={projects}/>
     <PortfolioCard name={name} aboutme={aboutme} />
   </div>
 };
