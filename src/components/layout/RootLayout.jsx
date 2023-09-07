4d4445771435608a25a2eeb644a5a9e3df232c45
Rajapaksha Pathirannehelage Chanaru Sampath <chanaru.rajapaksha@CLSGLPM220042.local>
2023-09-07 18:12:28 +0530

    feat: added header navbar

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -1,23 +1,28 @@
 import { Outlet, Link } from "react-router-dom";
+import "./RootLayout.css";
 
 export const RootLayout = () => {
   return (
-    <>
-      <header>
-        <nav>
-          <div>John Doe</div>
-          <ul>
-            <li>
+    <div className="container">
+      <header className="header">
+        <nav className="navbar">
+          <div className="logo">John Doe</div>
+          <ul className="nav-links">
+            <li className="nav-link">
               <Link to="home">Home</Link>
+            </li>
+            <li className="nav-link">
               <Link to="portfolio">Portfolio</Link>
+            </li>
+            <li className="nav-link">
               <Link to="technologies">Technologies</Link>
             </li>
           </ul>
         </nav>
       </header>
-      <main>
+      <main className="main">
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
