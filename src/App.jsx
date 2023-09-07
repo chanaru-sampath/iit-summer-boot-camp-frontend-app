fc24c9d54dcf34edbdfba0db67caf2a49f2d3f16
Gayan Kumarage <gayan.kumarage@circles.asia>
2023-09-07 17:30:53 +0530

    feat: react router dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { RootLayout } from "./components/layout/RootLayout";
+import Home from "./pages/home/Home"
 import "./App.css";
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Hello World</div>;
+  return <RouterProvider router={router} />
 }
 
 export default App;
